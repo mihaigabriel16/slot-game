2469fb7685cb786b901cd664cf9bcc6b4072aeb6
mihaigabriel16 <mihai.gabriel1608@gmail.com>
2022-09-23 20:51:54 +0300

    initialize

---
+++ src/scripts/globals/global_funcs.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+func setSizeProc(control, x, y):
+	var sizex = x
+	var sizey = y
+	control.set_size(Vector2(sizex, sizey))
---
+++ src/scripts/scenes/gameplay.gd
@@ -0,0 +1,8 @@
+extends Control
+
+
+
+func _ready():
+	var vp_size = get_viewport().get_size()
+	print(str(vp_size))
+
